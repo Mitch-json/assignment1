63ff8323e46753aa8f8abfbccc8e645fb237bb98
mitch-json-2 <mitchmacharia@students.uonbi.ac.ke>
2024-02-19 17:46:21 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("what is your name?")
+age = input("what is your age?")
+location = input("what is your location?")
+
+print("hello "+name+" ,you turned "+age+" this year and you are located at "+location)
